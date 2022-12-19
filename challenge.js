fe68aac34e3f3b9bd193b9516d2da9c2f24b54d5
Tarun teja <tarunsraina483@gmail.com>
2022-12-19 18:47:36 +0530

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,30 +1,183 @@
 const e = require("express");
 
 let input = [
-    ["or", true, ["and", false]],
-    ["and", true],
-    ["and", false],
-    ["or", true, true],
-    ["and", true, ["or", false, false, true], false]
-];
-
+  ["or", false],
+  [
+    "or",
+    [
+      "or",
+      [
+        "and",
+        true,
+        true,
+        ["or", false, true, true],
+        ["and", true, false, false, true, false, false],
+        ["and", true, false],
+        false,
+        true
+      ],
+      [
+        "or",
+        ["and", false, false, false, false, true],
+        true,
+        ["and", true, true, false, false, true, true]
+      ],
+      [
+        "and",
+        ["and", false, true],
+        false,
+        false,
+        ["and", true, false, false, true]
+      ],
+      true,
+      true
+    ],
+    true,
+    true,
+    true
+  ],
+  ["or", false],
+  [
+    "and",
+    [
+      "and",
+      true,
+      [
+        "and",
+        false,
+        false,
+        false,
+        ["and", false, true],
+        ["and", false, true, true],
+        false,
+        false
+      ],
+      false,
+      true,
+      ["and", false, true, false, true, true]
+    ],
+    false,
+    true,
+    false,
+    ["or", false],
+    false,
+    [
+      "or",
+      ["or", true, true, true, true, true, true, true],
+      true,
+      false,
+      ["or", false, false, true, ["or", true, true, true], false, false],
+      true,
+      ["and", ["or", true, true, false, false], ["or", true]],
+      false
+    ]
+  ],
+  [
+    "or",
+    false,
+    true,
+    false,
+    [
+      "and",
+      false,
+      true,
+      true,
+      [
+        "or",
+        false,
+        ["and", true, true, true],
+        ["and", true, true, true],
+        false,
+        false,
+        ["or", false, true, true, true, false, true],
+        ["and", true, true, true]
+      ]
+    ],
+    [
+      "and",
+      false,
+      false,
+      false,
+      false,
+      true,
+      false,
+      ["and", true, ["and", false, true], ["or", true]]
+    ],
+    [
+      "or",
+      false,
+      true,
+      ["and", false, true, true],
+      ["and", false],
+      true,
+      false,
+      [
+        "and",
+        true,
+        ["and", false, true, false],
+        false,
+        ["and", false, false, false, true],
+        true
+      ]
+    ],
+    [
+      "and",
+      false,
+      [
+        "or",
+        ["or", false, true, false],
+        ["or", false, false, true, true, true],
+        false,
+        true,
+        false,
+        ["or", true, true, false, false, true, true]
+      ],
+      false,
+      true,
+      true
+    ],
+    [
+      "or",
+      [
+        "and",
+        ["and", false, true, true, false],
+        true,
+        ["and", false, false, false, false]
+      ],
+      ["or", true],
+      true,
+      true,
+      ["or", true, false, ["and", true, false, true]],
+      false
+    ]
+  ]
+]
 let ans = []
+console.log("hereeeeeeeeeee")
+ console.log(typeof input[0][1]);
+console.log(input.length)
 
 function solver(inputArr) {
     
     let condition = inputArr[0];
 
+    let j=0;
     if(condition=="or"){
         for(let i=1;i<inputArr.length;i++){
-            if(inputArr[i]==true){
+            if(inputArr[i]==true){   
                 return true;
+            }else if(typeof inputArr[i]=='object'){
+              return solver(inputArr[i])
             }
         }
         return false;
     }else{
         for(let i=1;i<inputArr.length;i++){
+            
             if(inputArr[i]==false){
                 return false;
+            }else if(typeof inputArr[i]=='object'){
+              return solver(inputArr[i])
             }
         }
         return true;
