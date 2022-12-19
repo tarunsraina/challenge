8fa6d5b223c7778dab02d538f2377b38987652be
Tarun teja <tarunsraina483@gmail.com>
2022-12-19 19:09:12 +0530

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,155 +1,206 @@
 const e = require("express");
 
 let input = [
-  ["or", false],
   [
     "or",
+    [
+      "or",
+      false,
+      true,
+      false,
+      ["and", true, true, true, true, false, false, false],
+      [
+        "or",
+        true,
+        false,
+        ["or", true, true, true, true, true, false],
+        true,
+        ["and", true, false, false, false]
+      ]
+    ],
+    false,
+    false
+  ],
+  ["or", true],
+  [
+    "and",
+    false,
+    false,
+    ["and", false],
+    true,
+    false,
+    false,
+    true,
+    [
+      "and",
+      ["or", ["or", true, true, false, true, false, true], false],
+      true,
+      ["and", false, false, ["and", true, true, true, false, false, false]]
+    ],
+    false
+  ],
+  [
+    "and",
+    true,
+    [
+      "and",
+      false,
+      [
+        "or",
+        true,
+        true,
+        ["and", true, false, false, true],
+        false,
+        ["and", false, false, false],
+        ["or", false, true, false],
+        ["or", false, false, false, false]
+      ],
+      false
+    ],
+    true,
+    false,
     [
       "or",
       [
         "and",
         true,
-        true,
-        ["or", false, true, true],
-        ["and", true, false, false, true, false, false],
-        ["and", true, false],
         false,
+        ["and", false, true, false, false, false, false],
+        ["and", false, true],
         true
       ],
+      ["or", false, ["or", false, true, false, true, true]],
       [
         "or",
-        ["and", false, false, false, false, true],
+        ["or", false, false, true],
+        false,
+        ["or", true, true, true, false],
+        false,
         true,
-        ["and", true, true, false, false, true, true]
-      ],
+        true
+      ]
+    ],
+    false,
+    true,
+    false
+  ],
+  [
+    "or",
+    ["or", ["and", ["and", true, true]], true, true, false, false],
+    true,
+    [
+      "and",
       [
         "and",
-        ["and", false, true],
+        true,
+        ["and", true, true, true, true, true],
         false,
-        false,
-        ["and", true, false, false, true]
+        ["and", false, true, true]
       ],
+      true,
       true,
       true
     ],
-    true,
-    true,
-    true
+    false
   ],
-  ["or", false],
+  [
+    "or",
+    [
+      "and",
+      ["and", false, ["and", false, true, true, false, false], false, false],
+      false,
+      true,
+      false,
+      ["and", true, false, true],
+      true,
+      true
+    ]
+  ],
   [
     "and",
     [
-      "and",
-      true,
-      [
-        "and",
-        false,
-        false,
-        false,
-        ["and", false, true],
-        ["and", false, true, true],
-        false,
-        false
-      ],
-      false,
-      true,
-      ["and", false, true, false, true, true]
-    ],
-    false,
-    true,
-    false,
-    ["or", false],
-    false,
-    [
       "or",
-      ["or", true, true, true, true, true, true, true],
-      true,
-      false,
-      ["or", false, false, true, ["or", true, true, true], false, false],
-      true,
-      ["and", ["or", true, true, false, false], ["or", true]],
-      false
-    ]
-  ],
-  [
-    "or",
-    false,
-    true,
-    false,
-    [
-      "and",
-      false,
-      true,
-      true,
       [
         "or",
+        ["or", false, true, true],
         false,
-        ["and", true, true, true],
-        ["and", true, true, true],
-        false,
-        false,
-        ["or", false, true, true, true, false, true],
-        ["and", true, true, true]
-      ]
-    ],
-    [
-      "and",
-      false,
-      false,
-      false,
-      false,
-      true,
-      false,
-      ["and", true, ["and", false, true], ["or", true]]
-    ],
-    [
-      "or",
-      false,
-      true,
-      ["and", false, true, true],
-      ["and", false],
-      true,
-      false,
-      [
-        "and",
-        true,
-        ["and", false, true, false],
-        false,
-        ["and", false, false, false, true],
+        ["or", true, true, true],
+        ["and", false],
+        ["or", true, false, true, false],
+        ["or", false, true, true, true],
         true
-      ]
-    ],
-    [
-      "and",
-      false,
-      [
-        "or",
-        ["or", false, true, false],
-        ["or", false, false, true, true, true],
-        false,
-        true,
-        false,
-        ["or", true, true, false, false, true, true]
       ],
       false,
+      false
+    ],
+    true,
+    true,
+    [
+      "and",
       true,
+      ["and", false, false, false, false],
+      [
+        "and",
+        ["or", false, false],
+        true,
+        true,
+        true,
+        ["or", false, true, false, false, true, true]
+      ],
+      true,
+      ["or", ["or", true, false, true, true], false, true],
+      false
+    ],
+    false,
+    true,
+    ["and", true, true, true]
+  ],
+  ["and", ["and", false, true, true], false, true, true, true, false],
+  [
+    "and",
+    false,
+    [
+      "or",
+      ["and", false, false, true, ["and", false, true, true]],
+      [
+        "and",
+        false,
+        ["or", true, false, true, true],
+        ["or", true, true],
+        ["or", true]
+      ],
+      ["and", true, ["and", false], ["and", true, true], true, true, true],
+      ["and", ["and", true, false], true],
+      false,
       true
     ],
     [
-      "or",
+      "and",
+      ["and", true],
+      true,
+      [
+        "or",
+        false,
+        ["and", true, true, true, false, false, false],
+        ["or", false, false, true],
+        true,
+        true
+      ],
+      true,
       [
         "and",
-        ["and", false, true, true, false],
         true,
-        ["and", false, false, false, false]
+        true,
+        ["and", true, true, true],
+        ["and", false],
+        false,
+        false,
+        true
       ],
-      ["or", true],
-      true,
-      true,
-      ["or", true, false, ["and", true, false, true]],
-      false
-    ]
+      true
+    ],
+    true,
+    false
   ]
 ]
 let ans = []
@@ -176,8 +227,6 @@ function solver(inputArr) {
             
             if(inputArr[i]==false){
                 return false;
-            }else if(typeof inputArr[i]=='object'){
-              return solver(inputArr[i])
             }
         }
         return true;
